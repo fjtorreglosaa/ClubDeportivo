1fb0b6425a8b0e4a2214ec3f6ddd8cc2a6c73c38
fjtorreglosaa <59806344+fjtorreglosaa@users.noreply.github.com>
2022-05-03 16:54:54 -0500

    Modificacion del proyecto inicial

--- Entidades/Jugador/clsDelantero.java
+++
@@ -1,5 +0,0 @@
-package Entidades.Jugador;
-
-public class clsDelantero {
-    
-}
---
+++ Entidades/Jugador/clsPortero.java
@@ -0,0 +1,13 @@
+package Entidades.Jugador;
+
+public class clsPortero extends clsJugador{
+
+    private int paradasDeGol;
+    private int golesRecibidos;
+
+    public clsPortero(int id, String nombre, String apellido, int edad, float salario) {
+        super(id, nombre, apellido, edad, salario);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
